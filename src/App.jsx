b9bf5d9d5e098a5a34828ee47ad51f7fc40a05d1
Nancy142006 <nancy14kapoor2006@gmail.com>
2025-05-31 13:30:43 +0530

    imported portfolio

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Portfolio from "./pages/Portfolio";
+function App() {
+  return (
+    <div className="w-full h-screen bg-black">
+      <Portfolio/>
+    </div>
+  );
+}
+
+export default App;
